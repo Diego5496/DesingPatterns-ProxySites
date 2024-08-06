c7e21f5a6ea58de8e6cb9f2ca583020a0a51da50
Diego Moraes dos Santos Silva <diego.moraes@grupoaguasdobrasil.com.br>
2024-08-05 22:40:34 -0300

    Adicionar arquivos de projeto.

---
+++ Proxy_Solucao/RealInternet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Proxy_Solucao
+{
+    //RealSubject
+    public class RealInternet : Internet
+    {
+        public override void Conexao(string host)
+        {
+            Console.WriteLine($"Conectando-se com o servidor : {host}\n");
+        }
+    }
+}
